1b97a6b659630563e4d7c0d194d6e6db34fd47a0
Brandon Choi <jc440@duke.edu>
2015-08-03 23:17:20 -0700

    added overlay text on thumbnails

--- js/index.js
+++ js/index.js
@@ -1,13 +1,9 @@
 //Hover effect on thumbnails
-
 $(".photo-container").hover(
 	function() {
-		var d = $(this).find("img").attr("description");
-		$(this).find("img").css({"opacity":"0.4"});
-		$(this).find("img").css({"border":"3px solid #E697A2", "border-opacity":"1"});
+		$(this).find("img").css({"opacity":"0.45"});
 	},
 	function() {
 		$(this).find("img").css({"opacity":"1"});
-		$(this).find("img").css({"border":"none"});
 	}
 );
