b56d126b0e66532b96174a24d3f09f841bb6e453
Brandon Choi <jc440@duke.edu>
2015-08-07 12:36:30 -0700

    about me complete

---
+++ js/about.js
@@ -0,0 +1,17 @@
+/* toggle effects for change in picture */
+$("#young").click(function() {
+	changePhoto("maddie-young.jpg");
+});
+
+$("#old").click(function() {
+	changePhoto("maddie-old.jpg");
+});
+
+/* fade out & fade in with new photo */
+function changePhoto(path) {
+	var time = 300;
+	$("#profile-picture").fadeOut(time, function () {
+		$("#profile-picture").attr("src",path);
+		$("#profile-picture").fadeIn(time*3.5);
+	});
+}
