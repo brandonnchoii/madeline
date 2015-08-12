a2a0203b35c6cb53a0c9dc43d091df301f7dc60f
Brandon Choi <jc440@duke.edu>
2015-08-12 13:45:43 -0700

    all shrunken images and updated front end

---
+++ js/photo.js
@@ -0,0 +1,9 @@
+//hover effect on thumbnails
+$(".photo-container").hover(
+	function() {
+		$(this).find("img").css({"opacity":"0.45"});
+	},
+	function() {
+		$(this).find("img").css({"opacity":"1"});
+	}
+);
