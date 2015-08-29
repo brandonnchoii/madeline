8c1698e3e3629474474ed765c6c02918fd543bb0
Brandon Choi <jc440@duke.edu>
2015-08-29 14:09:53 -0400

    photo additions and edits

--- js/about.js
+++ js/about.js
@@ -9,7 +9,7 @@ $("#old").click(function() {
 
 /* fade out & fade in with new photo */
 function changePhoto(path) {
-	var time = 300;
+	var time = 500;
 	$("#profile-picture").fadeOut(time, function () {
 		$("#profile-picture").attr("src",path);
 		$("#profile-picture").fadeIn(time*3.5);
