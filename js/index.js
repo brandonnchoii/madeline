c6c534b9aaec18352ee96b685e74067961d9b215
Brandon Choi <jc440@duke.edu>
2015-10-17 23:13:31 -0400

    new video uploads

--- js/index.js
+++
@@ -1,9 +0,0 @@
-//hover effect on thumbnails
-$(".photo-container").hover(
-	function() {
-		$(this).find("img").css({"opacity":"0.45"});
-	},
-	function() {
-		$(this).find("img").css({"opacity":"1"});
-	}
-);
